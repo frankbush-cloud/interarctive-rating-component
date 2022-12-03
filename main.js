19bf69fadc0ee4451706220d83b8eb34c9f54f4c
frankbush-cloud <frankbush979@gmail.com>
2022-12-03 13:57:19 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const mainContainer = document.querySelector('.main-container');
+const thanksContainer = document.querySelector('.thankyou');
+const submitButton = document.getElementById('btn-submit');
+const rateAgain = document.getElementById('rate-again');
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+
+submitButton.addEventListener("click", ()=>{
+    thanksContainer.classList.remove("hidden");
+    mainContainer.style.display = "none";
+})
+ 
+rateAgain.addEventListener("click", ()=> {
+  thanksContainer.classList.add("hidden");
+  mainContainer.style.display="block";
+})
+
+rates.forEach((rate)=>{
+  rate.addEventListener("click", ()=>{
+    rating.innerHTML = rate.innerHTML;
+  })
+})
+  
