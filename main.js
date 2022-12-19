869e954d2a3b459020d1a5d047557454b72ce3ef
frankbush-cloud <frankbush979@gmail.com>
2022-12-19 15:34:07 +0300

    The commit is about appending the submit button and the rate again button in the for each loop

--- main.js
+++ main.js
@@ -6,20 +6,41 @@ const rateAgain = document.getElementById('rate-again');
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 
+//remove the main-container on clicking the submit-btton
+// submitButton.addEventListener("click", ()=>{
+//     thanksContainer.classList.remove("hidden");
+//     mainContainer.style.display = "none";
+// })
 
-submitButton.addEventListener("click", ()=>{
-    thanksContainer.classList.remove("hidden");
-    mainContainer.style.display = "none";
-})
- 
-rateAgain.addEventListener("click", ()=> {
-  thanksContainer.classList.add("hidden");
-  mainContainer.style.display="block";
-})
+// rateAgain.addEventListener("click", ()=> {
+//   thanksContainer.classList.add("hidden");
+//   mainContainer.style.display="block";
+// })
 
 rates.forEach((rate)=>{
   rate.addEventListener("click", ()=>{
     rating.innerHTML = rate.innerHTML;
+    submitButton.addEventListener("click", ()=>{
+      thanksContainer.classList.remove("hidden");
+      mainContainer.style.display = "none";
+      rateAgain.addEventListener("click", ()=> {
+        thanksContainer.classList.add("hidden");
+        mainContainer.style.display="block";
+      })
+  })
   })
 })
   
+
+// 
+// rates.forEach((rate)=>{
+//   rate.addEventListener("click", ()=>{
+//     rating.innerHTML = rate.innerHTML;
+//     submitButton.addEventListener("click", ()=>{
+//       thanksContainer.classList.remove("hidden");
+//       mainContainer.style.display = "none";
+//     })
+//   })
+// });
+
+
